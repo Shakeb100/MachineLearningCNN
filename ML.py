13bec382d12840e1f7e0f4f2be7af6ace4e9d3a1
Shakeb Siddiqui <smsicon2001@gmail.com>
2023-06-21 19:25:25 -0400

    added accuracy

--- ML.py
+++ ML.py
@@ -8,6 +8,21 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
+import threading
+import itertools
+import time
+
+
+
+# Function for loading symbol
+def loading_symbol():
+    symbols = itertools.cycle('\\|/-')  # Rotating line
+    while not thread_stop_event.is_set():
+        print('\r' + next(symbols), end='', flush=True)
+        time.sleep(0.1)  # Adjust this to change the speed of rotation
+
+
+
 # Get data 
 train = datasets.MNIST(root="data", download=True, train=True, transform=ToTensor())
 dataset = DataLoader(train, 32)
@@ -42,28 +57,13 @@ lossfn = nn.CrossEntropyLoss()
 if __name__ == "__main__":
     with open('model_state.pt', 'rb') as f:
         clf.load_state_dict(load(f)) #loads weights into clasifier
-    
-    
-    img = Image.open('985.jpg') #call jpeg file here 
-
-    img_tensor = ToTensor()(img).unsqueeze(0).to('cpu') #convert to a tensor
-
-    print("The predicted result is" , (torch.argmax(clf(img_tensor)))) #return number image
-
-
-
-
-
-
-
-'''
-
-    Training model
 
     for epoch in range(10): #train for 10 epochs
+        correct = 0
+        total = 0
         for batch in dataset:
-            x,y = batch
-            x,y = x.to("cpu"), y.to("cpu")
+            x, y = batch
+            x, y = x.to("cpu"), y.to("cpu")
             yhat = clf(x)
             loss = lossfn(yhat, y)
 
@@ -71,12 +71,20 @@ if __name__ == "__main__":
             loss.backward()
             opt.step()
 
-        print(f"Epoch: :{epoch} loss is {loss.item()}") #epoch is the cycle of an entire dataset passed through the neural network
+            _, predicted = torch.max(yhat.data, 1)
+            total += y.size(0)
+            correct += (predicted == y).sum().item()
+
+        print(f"Epoch: {epoch}, Loss: {loss.item()}, Accuracy: {correct / total * 100}%") #epoch is the cycle of an entire dataset passed through the neural network
+
+    thread_stop_event.set()
+    load_thread.join()
 
     with open('model_state.pt', 'wb') as f:
-       # save(clf.state_dict(), f)
+        save(clf.state_dict(), f)
 
-'''
-    
+    img = Image.open('./Testingimages/985.jpg') #call jpeg file here 
+    img_tensor = ToTensor()(img).unsqueeze(0).to('cpu') #convert to a tensor
+    print("The predicted result is" , (torch.argmax(clf(img_tensor)))) #return number image
 
 
